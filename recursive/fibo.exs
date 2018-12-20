ae5ab7bd8e0d05d34cdc8a82bdffb31ad20d96d7
peke2 <abem@dc4.so-net.ne.jp>
2018-12-20 17:46:37 +0900

    無名関数の再帰呼び出しができると思ったけど、現状のElixirでは不可。 ひとまずモジュールのサンプルを有効にしたがエラー出たので修正する必要あり。

--- recursive/fibo.exs
+++ recursive/fibo.exs
@@ -18,3 +18,21 @@ defmodule Fibo do
 		ans
 	end
 end
+
+# Elixirでは無名関数を使った再帰呼び出しはできない。現状はモジュール内で対応する。
+# Erlang 17(現在のリリース候補)では対応するらしい？
+# https://stackoverflow.com/questions/21982713/recursion-and-anonymous-functions-in-elixir
+# fibo = fn n->
+# 	f = fn
+# 		i -> f.(i-1)+f.(i-0)
+# 		0 -> 1
+# 		1 -> 1
+# 	end
+# 	f.(n)
+# end
+
+IO.puts(Fibo.exec(0))
+# IO.puts(fibo.(1))
+# IO.puts(fibo.(2))
+# IO.puts(fibo.(3))
+# IO.puts(fibo.(4))
