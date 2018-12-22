2780ba686205384a9c4ad4b49d8c77fedf072342
peke2 <abem@dc4.so-net.ne.jp>
2018-12-22 14:04:56 +0900

    クロージャーの確認を追加。確かに関数設置場所の変数を参照できる。

--- func/closure.exs
+++ func/closure.exs
@@ -8,3 +8,17 @@ IO.puts(hello.("World").())
 # この形でも呼び出し可能
 say = hello.("World")
 IO.puts(say.())
+
+# もっと簡単に直感的に表す
+a = 5
+b = 3
+add = fn -> a + b end
+IO.puts(add.())   # 8
+
+# でも、これはできない(言語的な仕様らしい？)
+# https://stackoverflow.com/questions/21982713/recursion-and-anonymous-functions-in-elixir
+# sum = fn
+#   0 -> 0
+#   n -> n + sum.(n-1)
+# end
+# IO.puts(sum.(10))
