eb64dd9bfd30e7149c42117fe0f484727fd2db1a
peke2 <abem@dc4.so-net.ne.jp>
2018-12-24 17:20:51 +0900

    Mpa.fetch()の戻り値の確認を追加。

--- collection/map.exs
+++ collection/map.exs
@@ -8,3 +8,21 @@ IO.puts(atom[:ghi])
 #atom2 = %{:abc=>7, def: 8, :ghi=>9}	# 混在はダメっぽい
 atom2 = %{:abc=>7, :def=> 8, :ghi=>9}
 IO.puts(atom2[:def])
+
+
+a = if Map.has_key?(atom, :ghi) do
+  atom[:ghi]
+else
+  "no exists"
+end
+IO.puts("#{a}")
+
+# Map.fetchの戻り値の確認
+#a = Map.fetch(map, "b")   # 「exists」
+a = Map.fetch(map, "e")   # 「no exists」
+case a do
+  {:ok,_} -> IO.puts("exists")
+  :error -> IO.puts("no exists")
+end
+
+
