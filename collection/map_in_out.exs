07d5a45b545cc8a006ac174fc369f8621543b141
peke2 <abem@dc4.so-net.ne.jp>
2018-12-24 17:03:54 +0900

    関数内でのマップ要素追加を確認。

--- collection/map_in_out.exs
+++ collection/map_in_out.exs
@@ -5,8 +5,17 @@ output = fn map ->
   IO.puts("--------")
 end
 
-# 要素を追加
 map = %{1=>"a", 2=>"b", 3=>"c"}
 output.(map)
+# 要素を追加
 map = Map.put(map, 4, "d")
 output.(map)
+
+# 関数内で要素を追加して返す
+add = fn map ->
+  Map.put(map, 5, "e")
+end
+
+map = add.(map)
+output.(map)
+
