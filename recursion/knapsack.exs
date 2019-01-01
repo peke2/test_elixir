a60984df2a92db31c7138874cdf84383d5fd5761
peke2 <abem@dc4.so-net.ne.jp>
2019-01-01 16:24:18 +0900

    外部で作成したデータをファイル読み込みで使用するよう変更。 管理対象外のファイル指定を追加。

--- recursion/knapsack.exs
+++ recursion/knapsack.exs
@@ -1,28 +1,66 @@
 defmodule Knapsack do
-  def proc([], _w, v) do
-    v
+  def proc([], _w, v, memo) do
+    {v, memo}
   end
 
-  def proc(knaps, w, v) do
-    [k|ks] = knaps
+  def proc(knaps, w, v, memo \\ %{}) do
+    index = length(knaps)
+    value = if !Map.has_key?(memo, index) do
+      # 状態に合った価値が残っていなければ計算
+      [k|ks] = knaps
 
-    v0 = if (w - k[:w]) >= 0 do
-      # 自分を加えた場合
-      proc(ks, w-k[:w], v+k[:v])
+      {v0, memo} = if (w - k[:w]) >= 0 do
+        # 自分を加えた場合
+        proc(ks, w-k[:w], v+k[:v], memo)
+      else
+        {0, memo}
+      end
+
+      # 自分を加えなかった場合
+      {v1, memo} = proc(ks, w, v, memo)
+
+      # 価値の大きい方をとる
+      value = max(v0, v1)
+      if Map.has_key?(memo, index) do
+        max(memo[index], value)
+      else
+        value
+      end
     else
-      0
+      memo[index]
     end
-    # 自分を加えなかった場合
-    v1 = proc(ks, w, v)
-    if v0 > v1 do
-      v0
-    else
-      v1
-    end
+    {value, Map.put(memo, index, value)}
   end
 end
 
+# ファイルの各行を読み込んで、空白でパースしてタプルに入れる
+defmodule Reader do
+  def readLines(device) do
+    line = case IO.read(device, :line) do
+      {:error, _} -> nil
+      :eof -> nil
+      data -> List.to_tuple(String.split(data))
+    end
+    if line != nil do
+      [line | readLines(device)]
+    else
+      []
+    end
+  end
 
+  def read(fname) do
+    readLines(File.open!(fname))
+  end
+end
 
-knaps = [%{w: 1,v: 2},%{w: 3,v: 1},%{w: 2,v: 3}]
-IO.puts Knapsack.proc(knaps, 3, 0)
+# ファイルの行に定義された値を内部で使えるよう整形
+lines = Reader.read("knaps.txt")
+knaps = for l <- lines do
+  {w, v} = l
+  # 内部では整数値のマップで扱う
+  %{w: String.to_integer(w), v: String.to_integer(v)}
+end
+
+# knaps = [%{w: 1,v: 2},%{w: 3,v: 1},%{w: 2,v: 3}]
+{v, _} = Knapsack.proc(knaps, 15, 0)
+IO.puts v
